ebc3e7d7d35fe5a9e85cfbb1fdf4460c9b5f7f74
Erik Gaius Capistrano <gaiussensei@gmail.com>
2013-05-31 07:10:19 -0400

    Removed node-restful module.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 var application_root = __dirname,
     express = require("express"),
     path = require("path");
-//    restful = require('node-restful'),
-//    mongoose = restful.mongoose;
-var app = express();
+    //mongoose = require('mongoose');
+var app = module.exports = express();
+
+// Database
+// mongoose.connect('mongodb://' + process.env.IP +'/cutlassDB');
 
 // Config
 app.configure(function () {
@@ -19,8 +21,6 @@ app.configure(function () {
 app.get('/api', function (req, res) {
   res.send('The Cutlass API is up and running!');
 });
-
-// Register Models
 
 // Launch server
 app.listen(process.env.PORT, process.env.IP);
