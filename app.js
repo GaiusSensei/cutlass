79128c1675cd4da57fc0f9ed0e3d82b86c55caa4
Erik Gaius Capistrano <gaiussensei@gmail.com>
2013-05-31 05:31:50 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Initialize
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path"),
+    restful = require('node-restful'),
+    mongoose = restful.mongoose;
+var app = express();
+
+// Config
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+// Test API
+app.get('/api', function (req, res) {
+  res.send('The Cutlass API is up and running!');
+});
+
+// Register Models
+
+// Launch server
+app.listen(process.env.PORT, process.env.IP);
